15db9cacb1afb4e6be1965af1fb750efb9b8c2d2
KimCuong236 <23kimcuong@gmail.com>
2025-09-17 15:18:06 +0700

    up

--- src/common/middlewares/socket.ts
+++ src/common/middlewares/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000", {
+export const socket = io("https://dating-backend-nhel.onrender.com", {
   withCredentials: true,
   transports: ["websocket"],
 });
